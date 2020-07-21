711525b371fb8606e8397aec7d9a6a0ed8ba1239
Ankit Yadav <ankitjnyadav@gmail.com>
2020-07-21 20:46:58 +0530

    Little more on Dart

--- main.dart
+++ main.dart
@@ -1,7 +1,12 @@
 import 'basic.dart';
-import 'package:flutter/material.dart';
 
 main() {
-  ay();
-  runApp();
+  var x = ['hi', 1, 2, 'Di'];
+  var map_example = {
+    'id': 1,
+    'email': 'ay@ay.com',
+    'phone': 123456,
+  };
+  print(map_example['id']);
+  //print(x[1]);
 }
